113f63a2dc151bb39762e4b84ec5ca6ee7083086
k-puneeth <42130202+k-puneeth@users.noreply.github.com>
2018-11-22 21:35:19 +0530

    Add files via upload

---
+++ JDBC-Files/table/Borrower.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+
+public class Borrower{
+	String b_ID;
+	String bname;
+	String borrower_type;
+	// public Student() { }
+	// public Student (String s, int r){ rollno = r; name = s; }
+	// public String getName() { return name; }
+	// public void setName(String s){ name = s; }
+	// public int getRollno() { return rollno; }
+	// public void setRollno(int s){ rollno = s; }
+	public void print(){ System.out.println("b_ID=" + b_ID); System.out.println("bname=" + bname);System.out.println("borrower_type=" + borrower_type);}
+};
+
---
+++ JDBC-Files/table/Student.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+
+public class Student{
+	int student_ID;
+	String sname;
+	String program;
+	String rollno;
+	// public Student() { }
+	// public Student (String s, int r){ rollno = r; name = s; }
+	// public String getName() { return name; }
+	// public void setName(String s){ name = s; }
+	// public int getRollno() { return rollno; }
+	// public void setRollno(int s){ rollno = s; }
+	public void print(){ System.out.println("rollno=" + rollno); System.out.println("Name=" + sname);System.out.println("Program=" + program);System.out.println("student_ID=" + student_ID);}
+};
+
